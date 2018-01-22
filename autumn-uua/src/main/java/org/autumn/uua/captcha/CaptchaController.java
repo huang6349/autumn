6bbb751858250223a58fc41974157f1f430b737b
huang6349 <252683858@qq.com>
2018-01-22 14:30:06 +0800

    优化

--- autumn-uua/src/main/java/org/autumn/uua/captcha/CaptchaController.java
+++ autumn-uua/src/main/java/org/autumn/uua/captcha/CaptchaController.java
@@ -1,5 +1,10 @@
 package org.autumn.uua.captcha;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang.StringUtils;
+import org.autumn.commons.utils.ResultUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,14 +20,19 @@ import javax.servlet.http.HttpServletResponse;
 @RestController
 public class CaptchaController {
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
     /**
      * 注入授权验证码处理器的服务提供类，{@link CaptchaProcessorProvice}接口的实现
      */
     @Autowired
     private CaptchaProcessorProvice captchaProcessorProvice;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     /**
-     * 创建验证码，根据验证码的类型不同，调用不同的{@link CaptchaProcessorProvice}接口实现
+     * 创建授权验证码，根据验证码的类型不同，调用不同的{@link CaptchaProcessorProvice}接口实现
      *
      * @param request  请求
      * @param response 响应
@@ -31,6 +41,14 @@ public class CaptchaController {
      */
     @GetMapping("/captcha/{type}")
     public void create(HttpServletRequest request, HttpServletResponse response, @PathVariable String type) throws Exception {
-        captchaProcessorProvice.findCaptchaProcessor(type).create(new ServletWebRequest(request, response));
+        logger.error("调用[" + StringUtils.upperCase(type) + "]授权验证码处理器");
+        try {
+            captchaProcessorProvice.findCaptchaProcessor(type).create(new ServletWebRequest(request, response));
+        } catch (Exception e) {
+            response.setCharacterEncoding("UTF-8");
+            response.setContentType("application/json;charset=utf-8");
+            response.getWriter().write(objectMapper.writeValueAsString(ResultUtil.error(e.getMessage())));
+            return;
+        }
     }
 }
